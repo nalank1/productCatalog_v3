f74e3e19d929febdc3969cdb4c7d8916bed067b7
Nalan Kurnaz <nkurn026@uottawa.ca>
2023-07-04 13:48:50 -0400

    Committing the code

--- app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
+++ app/src/test/java/alirezamontazer/createbest/productcatalog_v3/ProductTest.java
@@ -2,6 +2,8 @@ package alirezamontazer.createbest.productcatalog_v3;
 
 
 import static org.junit.Assert.*;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ProductTest {
@@ -21,7 +23,7 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        assertEquals("Check the price of the product", 180, aProduct.getPrice());
+        Assert.assertEquals("Check the price of the product", 180, aProduct.getPrice(), 0.001);
     }
 
 }
